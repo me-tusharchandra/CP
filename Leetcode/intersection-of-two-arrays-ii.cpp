6686944c51e7e097f696af719d61ae2414b5f478
Tushar Chandra <64960756+me-tusharchandra@users.noreply.github.com>
2023-03-16 23:44:08 +0530

    cp

---
+++ Leetcode/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,33 @@
+//https://leetcode.com/problems/intersection-of-two-arrays-ii/description/?envType=study-plan&id=data-structure-i
+
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size();
+        int n2 = nums2.size();
+
+        vector<int> v1;
+
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+
+        int i = 0;
+        int j = 0;
+
+        while(i<n1 && j<n2){
+            if(nums1[i]<nums2[j]){
+                i++;
+            }
+            else if(nums1[i]>nums2[j]){
+                j++;
+            }
+            else if(nums1[i] == nums2[j]){
+                v1.push_back(nums1[i]);
+                i++;
+                j++;
+            }
+        }
+
+        return v1;
+    }
+};
