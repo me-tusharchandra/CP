ad0d440747f0097a7cb97f92c2772451d7d74bbf
Tushar Chandra <64960756+me-tusharchandra@users.noreply.github.com>
2023-03-29 22:42:03 +0530

    cp

---
+++ Leetcode/concatination-of-array.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/concatenation-of-array/description/
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> v;
+        for(int i=0; i<n; i++){
+            v.push_back(nums[i]);
+        }
+        for(int i=0; i<n; i++){
+            v.push_back(nums[i]);
+        }
+
+        return v;
+    }
+};
